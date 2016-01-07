2b01016366df48c7f852442c270ec73d360e0202
Maris Orbidans <maris.orbidans@agentsmutual.co.uk>
2016-01-07 20:34:14 +0000

    get avail tables

--- src/zole_ws_handler.erl
+++ src/zole_ws_handler.erl
@@ -1,5 +1,5 @@
 -module(zole_ws_handler).
--import(lists,[delete/2,nth/2,split/2,sort/2,map/2,nth/2,zip/2,member/2,reverse/1,foreach/2]).
+-import(lists,[delete/2,nth/2,split/2,sort/2,map/2,nth/2,zip/2,member/2,reverse/1,foreach/2,all/2]).
 -import(maps,[get/2,put/3,from_list/1,to_list/1,is_key/2,keys/1,get/3,update/3]).
 
 -behaviour(cowboy_websocket_handler).
@@ -17,7 +17,6 @@ init({tcp, http}, _Req, _Opts) ->
 
 websocket_init(_TransportName, Req, _Opts) ->
         lager:info("WS INIT ~n",[]),
-%	erlang:start_timer(1000, self(), <<"Hello!">>),
 	{ok, Req, {}}.
 
 websocket_handle({text, Msg}, Req, State) ->
@@ -27,19 +26,15 @@ websocket_handle({text, Msg}, Req, State) ->
 			end,
     {reply, {text, jsx:encode(transform(Reply))}, Req, NewState};
 websocket_handle(Data, Req, State) ->
-        io:format("WS handle ~p ~p ~n",[ Data, State]),
-	{ok, Req, State}.
+    lager:info("WS handle ~p ~p ~n",[ Data, State]),
+    {ok, Req, State}.
 
 websocket_info(Msg, Req, State) ->
-        lager:info("WS info ~p ~n",[Msg]),
-%	erlang:start_timer(1000, self(), <<"How' you doin'?", State/binary>>),
-%	{reply, {text, Msg}, Req, State};
-     X = transform(Msg),
-    lager:info("X = ~p~n ",[X]),
-    {reply, {text, jsx:encode(X)}, Req, State}.
+    lager:info("WS info ~p ~n",[Msg]),
+    {reply, {text, jsx:encode(transform(Msg))}, Req, State}.
 
 websocket_terminate(Reason, _Req, State) ->
-        io:format("WS TERMINATE ~p ~p~n",[Reason, State]),
+        lager:info("WS TERMINATE ~p ~p~n",[Reason, State]),
 	ok.
 
 handle([<<"login">>, N], {} = S) ->
@@ -84,6 +79,9 @@ handle([<<"pass">>], {joined, TablePid} = S) ->
 handle([<<"last_game">>], {joined, TablePid} = S) ->
     R = table_sup:last_game(TablePid),
     {response(R, last_game), S};
+handle([<<"tables">>], S) ->
+    spawn(?MODULE, get_tables, [self()]),
+    {response({ok}, tables), S};
 handle([<<"play">>, Crd], {joined, TablePid} = S) ->
     Cs = (catch decode_card(Crd)),
     case Cs of
@@ -108,6 +106,10 @@ handle([<<"save">>, Cds], {joined, TablePid} = S) ->
 handle(_, S) ->
     {{error, illegal_state}, S}.
 
+get_tables(Pid) ->
+    {ok, Tables} = admin:list_avail_tables(),
+    Pid ! {tables, Tables}.
+
 decode_card(Binary) when is_binary(Binary) ->
     list_to_atom(binary_to_list(Binary));
 decode_card([N, S]) when is_integer(N) ->
@@ -123,8 +125,11 @@ response({error, Msg}, Return) ->
 transform(Tuple) when is_tuple(Tuple) ->
     transform(tuple_to_list(Tuple));
 transform(Map) when is_map(Map) ->
-    transform(maps:to_list(Map));
+    transform(to_list(Map));
 transform(List) when is_list(List) ->
-    lists:map(fun zole_ws_handler:transform/1, List);
+    case all(fun erlang:is_integer/1, List) of
+	false -> map(fun zole_ws_handler:transform/1, List);
+	true -> list_to_binary(List)
+    end;
 transform(X) ->
     X.
